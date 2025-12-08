c2f04b44ac0ca3f65c16945fb7df19ba42b0a30d
Will1184 <brandonwilliammg@gmail.com>
2025-12-08 14:32:19 -0600

    chore(nexus-platform-eureka): configuracion inicial para el uso del eureka en el proyecto de microservicio

---
+++ nexus-platform-eureka/src/main/java/cssp/gob/sv/nexusplatformeureka/NexusPlatformEurekaApplication.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusPlatformEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusPlatformEurekaApplication.class, args);
+    }
+
+}
---
+++ nexus-platform-eureka/src/test/java/cssp/gob/sv/nexusplatformeureka/NexusPlatformEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusPlatformEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
