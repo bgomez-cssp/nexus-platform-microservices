9e793ecc584a3ba5054a02249fd6d271d31dad61
Will1184 <brandonwilliammg@gmail.com>
2025-12-08 14:26:19 -0600

    chore(nexus-platform-gateway): configuracion inicial para el uso del gateway en el proyecto de microservicio

---
+++ nexus-platform-gateway/src/main/java/cssp/gob/sv/nexusplatformgateway/NexusPlatformGatewayApplication.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusPlatformGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusPlatformGatewayApplication.class, args);
+    }
+
+}
---
+++ nexus-platform-gateway/src/test/java/cssp/gob/sv/nexusplatformgateway/NexusPlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusPlatformGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
