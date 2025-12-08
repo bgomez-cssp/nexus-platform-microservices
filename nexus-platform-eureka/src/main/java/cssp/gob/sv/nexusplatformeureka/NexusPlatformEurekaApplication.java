7a3f6b3783b6319e8301d53729a8903aff09e60b
Will1184 <brandonwilliammg@gmail.com>
2025-12-08 15:46:18 -0600

    feat(nexus-platform-eureka): habilitar y configurar automaticamente el servicio de registro

--- nexus-platform-eureka/src/main/java/cssp/gob/sv/nexusplatformeureka/NexusPlatformEurekaApplication.java
+++ nexus-platform-eureka/src/main/java/cssp/gob/sv/nexusplatformeureka/NexusPlatformEurekaApplication.java
@@ -2,7 +2,9 @@ package cssp.gob.sv.nexusplatformeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NexusPlatformEurekaApplication {
 
