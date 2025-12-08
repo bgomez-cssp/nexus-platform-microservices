c55e589b38d3a3f0cbb21a926602e52cb6110dfd
Will1184 <brandonwilliammg@gmail.com>
2025-12-08 14:56:51 -0600

    chore(nexus-platform-config): configuracion inicial para el uso de config server en el proyecto de microservicio

---
+++ nexus-platform-config/src/main/java/cssp/gob/sv/nexusplatformconfig/NexusPlatformConfigApplication.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusPlatformConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusPlatformConfigApplication.class, args);
+    }
+
+}
---
+++ nexus-platform-config/src/test/java/cssp/gob/sv/nexusplatformconfig/NexusPlatformConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusPlatformConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
