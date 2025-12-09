6a765256f32b5f1565776ee52af53ac7b968975e
Will1184 <brandonwilliammg@gmail.com>
2025-12-09 16:10:49 -0600

    chore(nexus-platform-spring-boot-admin): configuracion inicial para el uso codecentric en el proyecto de microservicio

---
+++ nexus-platform-spring-boot-admin/src/test/java/cssp/gob/sv/nexusplatformspringbootadmin/NexusPlatformSpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformspringbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusPlatformSpringBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
