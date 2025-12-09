f5fb671767e9b6ff8a378f4a4cc5c6ba8e827550
Will1184 <brandonwilliammg@gmail.com>
2025-12-09 16:11:48 -0600

    chore(nexus-platform-it-inventory): configuracion inicial para el micorservicio de inventario de la unidad de informatica

---
+++ nexus-platform-it-inventory/src/main/java/cssp/gob/sv/nexusplatformitinventory/NexusPlatformItInventoryApplication.java
@@ -0,0 +1,13 @@
+package cssp.gob.sv.nexusplatformitinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusPlatformItInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusPlatformItInventoryApplication.class, args);
+	}
+
+}
